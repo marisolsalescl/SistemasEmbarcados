87fac909fab3976ce21b721eace4762c363ca6b0
Marisol <marisolsales243@gmail.com>
2026-08-31 22:56:21 -0300

    entrada validada

--- genio.ino
+++ genio.ino
@@ -1,147 +1,117 @@
-// C++ code
-//
+int buttonPin1 = 3;
+int buttonPin2 = 4;
+int buttonPin3 = 5;
+int buttonPin4 = 6;
 
-int buttonPin0 = 1; // Mudado para o pino 6 (o pino 1 interfere na Serial)
-int buttonPin1 = 2; // Botão 1
-int buttonPin2 = 3; // Botão 2
-int buttonPin3 = 4; // Botão 3
-int buttonPin4 = 5; // Botão 4
+const int ledVermelho = 9;
+const int ledVerde = 10;
+const int ledAzul = 8;
+const int ledAmarelo = 7;
+const int ledDelay = 500;
 
-int entradaUsuario = -1;
+int sequencia[4] = {1,2,3,4}; //teste
+int passoAtual = 0;
+int limiteLED = 4; // Quantidade de passos para a rodada
 
-const int ledVermelho = 8;
-const int ledVerde = 9;
-const int ledAzul = 7;
-const int ledAmarelo = 6;
-const int ledDelay = 750;
-
-int sequencia[13];
-
-// Configuracao LEDs
-int limiteLED;
-
-// Protótipo da função
 int configuraBotao(int b1, int b2, int b3, int b4);
-
 void iniciaJogo();
-
 void piscaLED();
-
 void acendeLed(int cor);
+bool entradaJogador(int botaoPressionado, int sequenciaAtual);
 
-void setup()
-{
-    Serial.begin(9600); // Inicializa a comunicação Serial
+void setup() {
+    Serial.begin(9600);
     randomSeed(analogRead(A0));
 
-    // Configura botoes
-    pinMode(buttonPin0, INPUT_PULLUP);
     pinMode(buttonPin1, INPUT_PULLUP);
     pinMode(buttonPin2, INPUT_PULLUP);
     pinMode(buttonPin3, INPUT_PULLUP);
     pinMode(buttonPin4, INPUT_PULLUP);
 
-     // Inicia LEDs
     pinMode(ledVermelho, OUTPUT);
     pinMode(ledVerde, OUTPUT);
     pinMode(ledAzul, OUTPUT);
     pinMode(ledAmarelo, OUTPUT);
 
     iniciaJogo();
-
 }
 
-void loop()
-{
-    // Lê o botão pressionado no momento
+void loop() { 
     int botaoLido = configuraBotao(buttonPin1, buttonPin2, buttonPin3, buttonPin4);
-  
+
+    if (botaoLido != -1) {
+        Serial.print("Botao pressionado: ");
+        Serial.println(botaoLido);
+
+        acendeLed(botaoLido); 
+
+        if (entradaJogador(botaoLido, sequencia[passoAtual])) {
+            Serial.println("CORRETO!");
+            passoAtual++;
+          if(passoAtual == 4){
+            iniciaJogo();
+          }
+            }
+         else {
+            Serial.println("ERRADO! Reiniciando");
+            delay(1000);
+            iniciaJogo(); 
+        }
+
+        delay(300); // Debounce simples
+    }
 }
 
-void iniciaJogo()
-{
-  limiteLED = 4;
+void iniciaJogo() {
+    limiteLED = 1; // Começa com 1 passo
+    passoAtual = 0;
+    Serial.print("sequencia");
+    for(int i = 0; i < 4; i++){
+        sequencia[i] = random(1, 5);
+      Serial.print(sequencia[i]);
+      
+    }
     
-  for(int i=0 ;i<13; i++){
-  	sequencia[i] = random(1,5);
-  }
-  
-  for(int i=0; i<13; i++){
-  	Serial.print(sequencia[i]);
-    Serial.print(" ");
-  }
-
-   piscaLED();
+    piscaLED();
 }
 
-int configuraBotao(int b1, int b2, int b3, int b4) 
-{
-  int clique = -1; // -1 significa nenhum botão pressionado
-
-  if (digitalRead(b1) == LOW) {
-    clique = 1;
-  }
-  else if (digitalRead(b2) == LOW) {
-    clique = 2;
-  }
-  else if (digitalRead(b3) == LOW) {
-    clique = 3;
-  }
-  else if (digitalRead(b4) == LOW) {
-    clique = 4;
-  }
-
-  return clique;
+int configuraBotao(int b1, int b2, int b3, int b4) {
+    if (digitalRead(b1) == LOW) return 1;
+    if (digitalRead(b2) == LOW) return 2;
+    if (digitalRead(b3) == LOW) return 3;
+    if (digitalRead(b4) == LOW) return 4;
+    return -1;
 }
 
-void piscaLED()
-{
-  for (int i = 0; i < limiteLED; i++)
-  {
-    delay(ledDelay);
-
-    acendeLed(sequencia[i]);
-  }
+void piscaLED() {
+    for (int i = 0; i < 4; i++) {
+        delay(300);
+        acendeLed(sequencia[i]);
+    }
 }
 
-void acendeLed(int cor)
-{
-  int led;
+void acendeLed(int cor) {
+    int led = 0;
+    switch (cor) {
+        case 1: led = ledVermelho; break;
+        case 2: led = ledVerde; break;
+        case 3: led = ledAzul; break;
+        case 4: led = ledAmarelo; break;
+    }
 
-  switch (cor)
-  {
-    case 1:
-      led = ledVermelho;
-      break;
-
-    case 2:
-      led = ledVerde;
-      break;
-
-    case 3:
-      led = ledAzul;
-      break;
-
-    case 4:
-      led = ledAmarelo;
-      break;
-  }
-
-
-  digitalWrite(led, HIGH);
-
-  delay(ledDelay);
-
-  digitalWrite(led, LOW);
+    if (led != 0) {
+        digitalWrite(led, HIGH);
+        delay(ledDelay);
+        digitalWrite(led, LOW);
+    }
 }
-bool entradaJogador(int botaoPressionado, int sequencia) {
-  int botaoPressionado = configuraBotao();
 
-  if (botaoPressionado == sequencia) {
-    // acender o led
-    return true; // Entrada correta
-  } 
-  else {
-    return false; // Entrada incorreta
+bool entradaJogador(int botaoPressionado, int sequenciaAtual) {
+  if(botaoPressionado == sequenciaAtual){
+    acendeLed(sequenciaAtual);
+    return true;
+  }else {
+    acendeLed(sequenciaAtual);
+    return false;
   }
 }
