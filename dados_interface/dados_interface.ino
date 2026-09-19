750a2a68baf56f23bdf74ed929c4e2d2f671c3a1
José Lucas de Lima Moura <135260328+jllm1@users.noreply.github.com>
2026-09-19 01:24:49 -0300

    Adicionado leitura LDR e modificado a logica display
    Agora o display atualiza so o valor da grandeza medida

--- dados_interface/dados_interface.ino
+++ dados_interface/dados_interface.ino
@@ -3,7 +3,7 @@
 Adafruit_LiquidCrystal lcd(0);
 
 int temperaturaNTC = 30;
-int luminosidadeLDR = 10;
+int luminosidadeLDR;
 int temperaturaDHT = 40;
 int luminosidadeDHT = 15;
 
@@ -11,6 +11,8 @@ int buttonTela = 2;
 int buttonPause = 3;
 
 int telaAtual = 1;
+int telaAnterior = 0;
+
 int estBotaoAnterior = HIGH;
 
 void setup() {
@@ -18,13 +20,16 @@ void setup() {
   pinMode(buttonPause, INPUT_PULLUP);
   temperaturaNTC = 
   lcd.begin(16, 2);
-  
-  desenharTela1();
+
 }
 
 void loop() {
-  int estBotaoAtual = digitalRead(buttonTela);
   
+  int LDR = analogRead(A0);
+  luminosidadeLDR = map(LDR, 0, 679, 0, 100);
+  
+  int estBotaoAtual = digitalRead(buttonTela);
+
   if(estBotaoAtual == LOW && estBotaoAnterior == HIGH){
     if(telaAtual == 1){
     	telaAtual = 2;
@@ -32,38 +37,62 @@ void loop() {
     else {
       telaAtual = 1;
     }
-    
-    lcd.clear();
+    delay(50);
+  }
+  estBotaoAnterior = estBotaoAtual;
+   
+  if(telaAtual != telaAnterior){
     if(telaAtual == 1){
-    	desenharTela1();
+      desenharLayoutTela1();
     } 
     if(telaAtual == 2) {
-      desenharTela2();
+      desenharLayoutTela2();
     }
+    telaAnterior = telaAtual;
   }
   
+  if (telaAtual == 1){
+  	atualizarValoresTela1();
+  } else if (telaAtual == 2){
+    atualizarValoresTela2();
+  }
   
-  delay(100); 
+  delay(100);
 }
 
-void desenharTela1() {
+void desenharLayoutTela1() {
   lcd.setCursor(0, 0);
   lcd.print("Temp NTC: "); 
-  lcd.print(temperaturaNTC);
-  lcd.print("C");
-  
+
   lcd.setCursor(0, 1);
   lcd.print("Lumi LDR: ");
-  lcd.print(luminosidadeLDR);
+
 }
 
-void desenharTela2() {
+void desenharLayoutTela2() {
   lcd.setCursor(0, 0);
   lcd.print("Temp DHT: "); 
-  lcd.print(temperaturaDHT);
-  lcd.print("C");
   
   lcd.setCursor(0, 1);
   lcd.print("Lumi DHT: ");
+}
+
+void atualizarValoresTela1() {
+  lcd.setCursor(10, 0);
+  lcd.print(temperaturaNTC);
+  lcd.print("C  ");
+
+  lcd.setCursor(10, 1);
+  lcd.print(luminosidadeLDR);
+  lcd.print("%  ");
+}
+
+void atualizarValoresTela2() {
+  lcd.setCursor(10, 0);
+  lcd.print(temperaturaDHT);
+  lcd.print("C  ");
+
+  lcd.setCursor(10, 1);
   lcd.print(luminosidadeDHT);
+  lcd.print("%  ");
 }
